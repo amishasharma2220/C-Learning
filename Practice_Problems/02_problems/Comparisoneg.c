64edcd8dd51885cd2f0f5b8144f0d2b92b65c698
Amisha Sharma <amishasharma2320@gmail.com>
2026-07-03 14:00:55 +0530

    Added practice problems

---
+++ Practice_Problems/02_problems/Comparisoneg.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int age=18;
+    printf("%d\n",age>=18); //This will check if age is greater than or equal to 18 and return 1 if true and 0 if false which will be 1.
+    printf("%d\n",age<18); //This will check if age is less than 18 and return 1 if true and 0 if false which will be 0.
+    return 0;
+}
