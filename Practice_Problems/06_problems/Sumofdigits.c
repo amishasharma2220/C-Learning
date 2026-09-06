c9c32c2d39cf73fb06359fe479e5f1bb6138d715
Amisha Sharma <amishasharma2320@gmail.com>
2026-09-06 19:28:47 +0530

    Add :Practice Problem

--- Practice_Problems/06_problems/Sumofdigits.c
+++ Practice_Problems/06_problems/Sumofdigits.c
@@ -1,1 +1,18 @@
 //Sum of digits of a number using recursion
+#include<stdio.h>
+int sumOfDigits(int n);
+int main(){
+    printf("Enter a number:");
+    int n;
+    scanf("%d",&n);
+    int result=sumOfDigits(n);
+    printf("Sum of digits of %d is:%d\n",n,result);
+    return 0;
+
+}
+int sumOfDigits(int n){
+    if(n == 0){
+        return 0;
+    }
+    return (n%10)+sumOfDigits(n/10);
+}
